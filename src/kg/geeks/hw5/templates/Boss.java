3fb67527911032c7dd61f03b64eb2bad9e585652
emirkaramoldoev <emirkaramoldoev1@gmail.com>
2023-05-13 17:59:52 +0600

    first commit

---
+++ src/kg/geeks/hw5/templates/Boss.java
@@ -0,0 +1,32 @@
+package kg.geeks.hw5;
+
+public class Boss {
+    private int health;
+    private int damage;
+    private String defenceType;
+
+    public int getHealth(){return this.health;}
+    public int getDamage(){return this.damage;}
+    public String getUltimate(){return this.defenceType;}
+    public void setHealth(int newHealth){
+        if (newHealth > 0) {
+            this.health = newHealth;
+        } else {
+            System.out.println("Can't be zero or lower");
+        }
+    }
+    public void setDamage(int newDamage){
+        if (newDamage > 0) {
+            this.health = newDamage;
+        } else {
+            System.out.println("Can't be zero or lower");
+        }
+    }
+    public void setDefenceTypee(String newDefenceType){
+        if (newDefenceType != null) {
+            this.defenceType = newDefenceType;
+        } else {
+            System.out.println("Can't be empty");
+        }
+    }
+}
---
+++ src/kg/geeks/hw5/templates/Hero.java
@@ -0,0 +1,23 @@
+public class Hero {
+    private int health;
+    private int damage;
+
+    private String ultimate;
+
+    public Hero(int health, int damage, String ultimate) {
+        this.health = health;
+        this.damage = damage;
+        this.ultimate = ultimate;
+    }
+    public Hero(int health, int damage) {
+        this.health = health;
+        this.damage = damage;
+        this.ultimate = ultimate;
+    }
+
+    public int getHealth(){return this.health;}
+    public int getDamage(){return this.damage;}
+
+    public String getUltimate(){return this.ultimate;}
+}
+
